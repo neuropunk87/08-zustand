c3748350920771fe8def43a58c6d7cd6f101b8f1
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-09-08 13:49:59 +0300

    Added generateMetadata function to NoteDetails

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import {
   dehydrate,
   HydrationBoundary,
@@ -10,28 +9,6 @@ import NotePreviewClient from "./NotePreview.client";
 type Props = {
   params: Promise<{ id: string }>;
 };
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { id } = await params;
-  const note = await fetchNoteById(id);
-  return {
-    title: note.title,
-    description: note.content.slice(0, 30),
-    openGraph: {
-      title: note.title,
-      description: note.content.slice(0, 30),
-      url: `https://notehub.example.com/notes/${id}`,
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-          width: 1200,
-          height: 630,
-          alt: note.title,
-        },
-      ],
-    },
-  };
-}
 
 const NotePreview = async ({ params }: Props) => {
   const { id } = await params;
